7bb82588a176a234784a26cbf411e2e1345fcc5c
oOp <ossama.abboud95@gmail.com>
2026-05-27 11:46:43 -1100

    v0.1.2

--- src/trend.rs
+++ src/trend.rs
@@ -1,3 +1,5 @@
+use std::fmt::{Debug};
+
 /// Extension trait for converting two ordered values into a [`Trend`].
 ///
 /// # Examples
@@ -171,4 +173,101 @@ impl<'a, T> Trend<'a, T> {
             -1
         }
     }
+    /// Returns a reference to the 'min' point in the trend
+    pub fn min_point(&self) -> &'a T {
+        match self.direction() {
+            1 | 0 => self.start(),
+            -1 => self.end(),
+            _ => {
+                //unreachable block
+                //read documentation of `returns::return_shit!()`
+                //it will cause panic if reached in release mode
+                //but it is impossible to reach here
+                //read documentation of method `Trend::direction()`
+                returns::return_shit!()
+            }
+        }
+    }
+
+    /// Returns a reference to the 'max' point in the trend
+    pub fn max_point(&self) -> &'a T {
+        match self.direction() {
+            1 | 0 => self.end(),
+            -1 => self.start(),
+            _ => {
+                //unreachable block
+                //read documentation of `returns::return_shit!()`
+                //it will cause panic if reached in release mode
+                //but it is impossible to reach here
+                //read documentation of method `Trend::direction()`
+                returns::return_shit!()
+            }
+        }
+    }
+
+    /// Returns a cloned Copy `as Clone` to the 'min' point in the trend
+    ///
+    /// Requires `T:Clone`
+    pub fn min_point_cloned(&self) -> T
+    where
+        T: Clone,
+    {
+        match self.direction() {
+            1 | 0 => self.start().clone(),
+            -1 => self.end().clone(),
+            _ => {
+                //unreachable block
+                //read documentation of `returns::return_shit!()`
+                //it will cause panic if reached in release mode
+                //but it is impossible to reach here
+                //read documentation of method `Trend::direction()`
+                returns::return_shit!()
+            }
+        }
+    }
+
+    /// Returns a cloned Copy `as Clone` to the 'max' point in the trend
+    ///
+    /// Requires `T:Clone`
+    pub fn max_point_cloned(&self) -> T
+    where
+        T: Clone,
+    {
+        match self.direction() {
+            1 | 0 => self.end().clone(),
+            -1 => self.start().clone(),
+            _ => {
+                //unreachable block
+                //read documentation of `returns::return_shit!()`
+                //it will cause panic if reached in release mode
+                //but it is impossible to reach here
+                //read documentation of method `Trend::direction()`
+                returns::return_shit!()
+            }
+        }
+    }
+
+    //TODO:Fix for release version
+    #[allow(unused)]
+    fn sort_group(trends: &mut Vec<Self>) {
+        //sort each trend in the group
+        for i in 0..trends.len() {
+            trends[i].sort_unit();
+        }
+
+        //now units are sorted
+        //we need to sort the units as groups
+    }
+
+    //TODO:Fix for release version
+    #[allow(unused)]
+    fn sort_unit(&mut self) {
+        let min = self.min_point();
+        let max = self.max_point();
+        (*self) = Trend::Rising {
+            start: min,
+            end: max,
+        }
+    }
+    
 }
--- tests/trend_test.rs
+++ tests/trend_test.rs
@@ -111,4 +111,51 @@ mod tests {
         assert_eq!(2.to_trend(&1).direction(), -1);
         assert_eq!(1.to_trend(&1).direction(), 0);
     }
+
+    #[test]
+    fn min_test() {
+        let trend = 1.to_trend(&5);
+        let min = trend.min_point();
+        assert_eq!(min, &1);
+
+        let trend = 5.to_trend(&1);
+        assert_eq!(trend.min_point(), &1);
+    }
+
+    #[test]
+    fn max_test() {
+        let start = String::from("A");
+        let end = String::from("B");
+        let trend = start.to_trend(&end);
+        assert_eq!(trend.max_point(), &"B");
+
+        let trend = end.to_trend(&start);
+        assert_eq!(trend.max_point(), &"B")
+    }
+
+    #[test]
+    fn min_cloned() {
+        let start = String::from("A");
+        let end = String::from("B");
+        let trend = start.to_trend(&end);
+
+        let min_cloned = trend.min_point_cloned();
+        assert_eq!(min_cloned, "A");
+
+        let min_cloned = end.to_trend(&start).min_point_cloned();
+        assert_eq!(min_cloned, "A");
+    }
+
+    #[test]
+    fn max_cloned() {
+        let start = String::from("A");
+        let end = String::from("B");
+        let trend = start.to_trend(&end);
+
+        let max_cloned = trend.max_point_cloned();
+        assert_eq!(max_cloned, "B");
+
+        let max_cloned = end.to_trend(&start).max_point_cloned();
+        assert_eq!(max_cloned, "B");
+    }
 }
